e894e7c8f23693f86d7e16fea3bd488890513305
ANDOUNGOU NZIE Lyly <lylynzie@gmail.com>
2025-03-31 11:49:37 +0100

    Ajout Darktheme

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,25 @@
-let input=document.getElementById("check_cross")
+
+document.addEventListener("DOMContentLoaded", () => {
+  const darkModeButton = document.querySelector(".dark_mode");
+  let currentTheme = localStorage.getItem("theme") || "light";
+
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    darkModeButton.src = "./assets/imgs/soleil.svg";
+  }
+
+  darkModeButton.addEventListener("click", () => {
+    const newTheme =
+      document.documentElement.getAttribute("data-theme") === "dark"
+        ? "light"
+        : "dark";
+
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    darkModeButton.src =
+      newTheme === "dark"
+        ? "./assets/imgs/soleil.svg"
+        : "./assets/imgs/lune.svg";
+  });
+});
