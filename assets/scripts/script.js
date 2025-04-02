ef07d70c2513009f1868fba1b775e50cb97833b4
leonel691 <leonelngueaho@gmail.com>
2025-04-02 10:45:12 +0100

    option de cochage sur l'image ajouter

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const darkModeButton = document.querySelector(".dark_mode");
-  let currentTheme = localStorage.getItem("theme") || "light";
+  document.addEventListener("DOMContentLoaded", () => {
+    const darkModeButton = document.querySelector(".dark_mode");
+    let currentTheme = localStorage.getItem("theme") || "light";
 
-  if (currentTheme === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-    darkModeButton.src = "";
-  }
+    if (currentTheme === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
+      darkModeButton.src = "";
+    }
 
-  darkModeButton.addEventListener("click", () => {
-    const newTheme =
-      document.documentElement.getAttribute("data-theme") === "dark"
-        ? "light"
-        : "dark";
+    darkModeButton.addEventListener("click", () => {
+      const newTheme =
+        document.documentElement.getAttribute("data-theme") === "dark"
+          ? "light"
+          : "dark";
 
-    document.documentElement.setAttribute("data-theme", newTheme);
-    localStorage.setItem("theme", newTheme);
+      document.documentElement.setAttribute("data-theme", newTheme);
+      localStorage.setItem("theme", newTheme);
 
-    darkModeButton.src =
-      newTheme === "dark"
-        ? "./assets/imgs/soleil.jpg"
-        : "./assets/imgs/lune.svg";
+      darkModeButton.src =
+        newTheme === "dark"
+          ? "./assets/imgs/soleil.svg"
+          : "./assets/imgs/lune.svg";
+    });
   });
+
+
+
+
+// option de cochage
+let imagecoche=document.getElementById("inputcocher");
+let input=document.getElementById("check_input");
+input.addEventListener("input", function() {
+  console.log(input.value.length);
+  if (input.value.length > 4) {
+    console.log("super")
+    imagecoche.style.display="flex"
+
+  } 
+  else{
+    imagecoche.style.display="none"
+  }
 });
+  
